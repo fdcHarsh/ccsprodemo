9782d86e630781a32b908353d932e2ef39362e20
fdcHarsh <harsh@focusdesignconsulting.com>
2026-03-12 21:36:53 +0530

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -120,6 +121,7 @@ const App = () => (
                 <TooltipProvider>
                   <Toaster />
                   <Sonner />
+                  <Analytics />
                   <BrowserRouter>
                     <AppRoutes />
                   </BrowserRouter>
